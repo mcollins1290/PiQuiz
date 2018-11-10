25f43a359a5a645dcf4074e280dc652620513a10
Martin Collins <martin_p_coll@hotmail.com>
2018-11-09 22:30:14 -0500

    Hooray! Releasing v1.0.0 of PiQuiz :-)

--- PiQuiz.py
+++ PiQuiz.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3.7
 
 ##### GLOBAL VARIABLES #####
-PROG_VERSION = "0.1.0"
+PROG_VERSION = "1.0.0"
 PROG_NAME = "PiQuiz"
 SETTINGS = []
 LCD = None
 GPIO_INIT_DONE = False
 BUTTON_WAIT_DELAY = 0.3
 LED_WAIT_DELAY = 2
+BOUNCETIME=500
 MySQL_DB_Conn = None
 ##### CLI OPT VARIABLES ####
 DEBUG_ENABLED = False
@@ -39,9 +40,9 @@ def handle_args():
     global DEBUG_ENABLED
     global NON_GPIO_ENABLED
     global TEST_MODE_ENABLED
-    
+
     parser = argparse.ArgumentParser(description=PROG_NAME + ' - Multiple choice Question generator using a LCD screen and RPIs GPIO inputs.')
-    
+
     parser.add_argument('-v','--version',help='show version info and exit.', action="store_true")
     parser.add_argument('-n','--non-gpio',help='do not use RPIs GPIO inputs/outputs. Instead use keyboard for input and print output to CLI.', action="store_true")
     parser.add_argument('-t','--test',help='run tests to ensure RPIs GPIO inputs/outputs are working correctly.', action="store_true")
@@ -62,7 +63,7 @@ def handle_args():
         if DEBUG_ENABLED:
             print("TEST MODE SELECTED")
         TEST_MODE_ENABLED = True
-    
+
 def load_settings():
     global DEBUG_ENABLED
     global SETTINGS
@@ -78,11 +79,11 @@ def load_settings():
         sys.exit(1)
     # File exists, check sections and options are present. If not, print error to CLI and Exit.
     for section in [ 'MySQL', 'GPIO' ]:
-        
+
         if not config.has_section(section):
             print("ERROR: Missing settings section: " + section +". Please check " + settings_filename + ". Exiting...")
             sys.exit(1)
-        
+
         if section == 'MySQL':
             for option in [ 'Host', 'User', 'Password', 'Database' ]:
                 if not config.has_option(section, option):
@@ -96,7 +97,7 @@ def load_settings():
                     print("ERROR: Missing GPIO settings option: " + option +". Please check " + settings_filename + ". Exiting...")
                     sys.exit(1)
     # Settings file sections and options valid. Now retrieve/parse values and store in global dict
-    try:        
+    try:
         SETTINGS = {'MYSQL_HOSTNAME':config.get('MySQL', 'Host'),
                     'MYSQL_DATABASE':config.get('MySQL', 'Database'),
                     'MYSQL_USER':config.get('MySQL', 'User'),
@@ -116,10 +117,7 @@ def load_settings():
                     'GPIO_LCD_ROWS':config.getint('GPIO', 'LCD_ROWS'),
                     'GPIO_RED_LED':config.getint('GPIO', 'RED_LED'),
                     'GPIO_GREEN_LED':config.getint('GPIO', 'GREEN_LED')}
-        
         #Populate GPIO Input Options dict based on keys,values in SETTINGS dict
-        #GPIO_INPUT_OPTIONS = {k:v for k,v in SETTINGS.items() if 'OPTION' in k}
-        
         for key, value in SETTINGS.items():
             if 'OPTION' in key:
                 GPIO_INPUT_OPTIONS.append(value)
@@ -128,7 +126,7 @@ def load_settings():
             for key, value in SETTINGS.items():
                 print(key, value)
             print("INFO: GPIO Input Options list contains following keys & values:")
-            print(GPIO_INPUT_OPTIONS)   
+            print(GPIO_INPUT_OPTIONS)
 
     except ValueError as e:
         print("ERROR: Unable to parse values from settings file: \n" + str(e))
@@ -140,6 +138,7 @@ def init_gpio_lcd():
         global LCD
         global DEBUG_ENABLED
         global GPIO_INIT_DONE
+        global BOUNCETIME
         # Set GPIO reference mode
         GPIO.setmode(GPIO.BCM)
         # Setup GPIO LEDs
@@ -152,11 +151,11 @@ def init_gpio_lcd():
         GPIO.setup(SETTINGS['GPIO_OPTION_D_BUTTON'], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         GPIO.setup(SETTINGS['GPIO_QUIT_BUTTON'], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         # Add event triggers to Button GPIO channels
-        GPIO.add_event_detect(SETTINGS['GPIO_OPTION_A_BUTTON'], GPIO.RISING)
-        GPIO.add_event_detect(SETTINGS['GPIO_OPTION_B_BUTTON'], GPIO.RISING)
-        GPIO.add_event_detect(SETTINGS['GPIO_OPTION_C_BUTTON'], GPIO.RISING)
-        GPIO.add_event_detect(SETTINGS['GPIO_OPTION_D_BUTTON'], GPIO.RISING)
-        GPIO.add_event_detect(SETTINGS['GPIO_QUIT_BUTTON'], GPIO.RISING)
+        GPIO.add_event_detect(SETTINGS['GPIO_OPTION_A_BUTTON'], GPIO.RISING, bouncetime=BOUNCETIME)
+        GPIO.add_event_detect(SETTINGS['GPIO_OPTION_B_BUTTON'], GPIO.RISING, bouncetime=BOUNCETIME)
+        GPIO.add_event_detect(SETTINGS['GPIO_OPTION_C_BUTTON'], GPIO.RISING, bouncetime=BOUNCETIME)
+        GPIO.add_event_detect(SETTINGS['GPIO_OPTION_D_BUTTON'], GPIO.RISING, bouncetime=BOUNCETIME)
+        GPIO.add_event_detect(SETTINGS['GPIO_QUIT_BUTTON'], GPIO.RISING, bouncetime=BOUNCETIME)
         # Retrieve LCD GPIO pins from global SETTINGS dict.
         lcd_rs = SETTINGS['GPIO_LCD_RS']
         lcd_en = SETTINGS['GPIO_LCD_EN']
@@ -173,7 +172,7 @@ def init_gpio_lcd():
         if DEBUG_ENABLED:
             print(str(LCD))
             print("GPIO INIT COMPLETE? = ", GPIO_INIT_DONE)
-        
+
     except:
         print("ERROR: Unexpected error during INIT GPIO LCD:", sys.exc_info())
         GPIO.cleanup()
@@ -190,7 +189,7 @@ def run_gpio_tests():
         GPIO.output(SETTINGS['GPIO_RED_LED'], GPIO.HIGH)
         time.sleep(2)
         print("------ Switching on GREEN LED.")
-        GPIO.output(SETTINGS['GPIO_GREEN_LED'], GPIO.HIGH)        
+        GPIO.output(SETTINGS['GPIO_GREEN_LED'], GPIO.HIGH)
         time.sleep(2)
         print("------ Switching off LEDs.")
         GPIO.output(SETTINGS['GPIO_RED_LED'], GPIO.LOW)
@@ -221,7 +220,6 @@ def run_gpio_tests():
                 if GPIO.event_detected(SETTINGS['GPIO_QUIT_BUTTON']):
                     print("Quit Button Pressed")
                     BUTTON_QUIT_TESTED_OK=True
-                time.sleep(BUTTON_WAIT_DELAY)
         except KeyboardInterrupt:
             print("Leaving TEST 2. Moving onto next TEST...")
         if LCD == None:
@@ -243,18 +241,18 @@ def establish_db_connection():
         global MySQL_DB_Conn
         global DEBUG_ENABLED
         global SETTINGS
-        
+
         if DEBUG_ENABLED:
             print("INFO: Attempting to connect to " + SETTINGS['MYSQL_DATABASE'] + '@' + SETTINGS['MYSQL_HOSTNAME'] +
                   " using '" + SETTINGS['MYSQL_USER'] + "' as the User and '" + SETTINGS['MYSQL_PASSWORD'] + "' as the Password.")
-        
+
         MySQL_DB_Conn = mysql.connector.connect(host=SETTINGS['MYSQL_HOSTNAME'],
                                        database=SETTINGS['MYSQL_DATABASE'],
                                        user=SETTINGS['MYSQL_USER'],
                                        password=SETTINGS['MYSQL_PASSWORD'])
         if MySQL_DB_Conn.is_connected():
             print('Connected to ' + SETTINGS['MYSQL_DATABASE'] + '@' + SETTINGS['MYSQL_HOSTNAME'] +  ' MySQL database.')
-    
+
     except Error as e:
         print("ERROR: Error occurred while trying to connect to the MySQL database: ", str(e))
         sys.exit(1)
@@ -262,7 +260,7 @@ def establish_db_connection():
 def output(output_text,pause1=False, pause2=False, rep=False, printToConsole=False):
     global NON_GPIO_ENABLED
     global LCD
-    
+
     PAUSE_NEXT = 1
     PAUSE_REP = 1
     REPETITIONS = 2
@@ -270,7 +268,7 @@ def output(output_text,pause1=False, pause2=False, rep=False, printToConsole=Fal
     if pause1: PAUSE_NEXT = pause1
     if pause2: PAUSE_REP = pause2
     if rep: REPETITIONS = rep
-    
+
     if NON_GPIO_ENABLED or printToConsole:
         print(output_text)
     if not NON_GPIO_ENABLED:
@@ -297,7 +295,7 @@ def input_answer(question_id):
     global GPIO_INPUT_OPTIONS
     global LCD
     answers_input_dict = {}
-    
+
     cursor = MySQL_DB_Conn.cursor(named_tuple=True)
     query = ("""SELECT a.answer_id,a.answer_text
                 FROM answers a
@@ -314,22 +312,20 @@ def input_answer(question_id):
     #Map answers to Inputs
     for i,row in enumerate(answers):
         if NON_GPIO_ENABLED:
-            answers_input_dict[i+1] = {
+            answers_input_dict[i] = {
                     'answer_id': row.answer_id,
                     'answer_text': row.answer_text,
                     'input': NON_GPIO_INPUT_OPTIONS[i]}
         else:
-            answers_input_dict[i+1] = {
+            answers_input_dict[i] = {
                     'answer_id': row.answer_id,
                     'answer_text': row.answer_text,
                     'input': GPIO_INPUT_OPTIONS[i]}
-            print(answers_input_dict)
-            
     #Display answers to Player
     popts = "a", "b", "c", "d";
     msg = ''
     for i,row in enumerate(answers_input_dict):
-        msg = msg + popts[i] + ") " + answers_input_dict[i+1]['answer_text'] + " "
+        msg = msg + popts[i] + ") " + answers_input_dict[i]['answer_text'] + " "
     output(msg,1,2,2,False)
     #Get player input
     user_input = None
@@ -337,30 +333,39 @@ def input_answer(question_id):
         if NON_GPIO_ENABLED:
             user_input = input("Press the letter on your keyboard corresponding to the answer you wish to select and press ENTER...").upper()
         else:
-            LCD.message("Choose your answer...")
-            if GPIO.event_detected(SETTINGS['GPIO_OPTION_A_BUTTON']):
-                user_input = GPIO_INPUT_OPTIONS[0]
-            if GPIO.event_detected(SETTINGS['GPIO_OPTION_B_BUTTON']):
-                user_input = GPIO_INPUT_OPTIONS[1]
-            if GPIO.event_detected(SETTINGS['GPIO_OPTION_C_BUTTON']):
-                user_input = GPIO_INPUT_OPTIONS[2]
-            if GPIO.event_detected(SETTINGS['GPIO_OPTION_D_BUTTON']):
-                user_input = GPIO_INPUT_OPTIONS[3]
-            if GPIO.event_detected(SETTINGS['GPIO_QUIT_BUTTON']):
-                print("Quit Button Pressed")
+            LCD.message("Choose your \nanswer...")
+            while True:
+                if GPIO.event_detected(SETTINGS['GPIO_OPTION_A_BUTTON']):
+                    user_input = GPIO_INPUT_OPTIONS[0]
+                    break
+                if GPIO.event_detected(SETTINGS['GPIO_OPTION_B_BUTTON']):
+                    user_input = GPIO_INPUT_OPTIONS[1]
+                    break
+                if GPIO.event_detected(SETTINGS['GPIO_OPTION_C_BUTTON']):
+                    user_input = GPIO_INPUT_OPTIONS[2]
+                    break
+                if GPIO.event_detected(SETTINGS['GPIO_OPTION_D_BUTTON']):
+                    user_input = GPIO_INPUT_OPTIONS[3]
+                    break
+                if GPIO.event_detected(SETTINGS['GPIO_QUIT_BUTTON']):
+                    display_score(noofqcorrect,noofqincorrect,noofqasked)
+                    output("Thanks for playing. Good-Bye!",1,2,1,True)
+                    MySQL_DB_Conn.close()
+                    GPIO.cleanup()
+                    sys.exit(0)
         #Validate user input
         for i,row in enumerate(answers_input_dict):
-            if user_input == answers_input_dict[i+1]['input']:
-                msg = ("You selected Answer " + answers_input_dict[i+1]['input'].upper() + " (" + answers_input_dict[i+1]['answer_text'] + ")")
+            if user_input == answers_input_dict[i]['input']:
+                msg = ("You selected Answer " + popts[i].upper() + " (" + answers_input_dict[i]['answer_text'] + ")")
                 output(msg,1,2,1,False)
-                return answers_input_dict[i+1]['answer_id']
+                return answers_input_dict[i]['answer_id']
         #If answer from user not valid, display appropiate message
         print("Input invalid. Try again")
-        
+
 def illuminate_lcd(gpio_output,sleep_time):
-    GPIO.output(ggpio_output, GPIO.HIGH)
+    GPIO.output(gpio_output, GPIO.HIGH)
     time.sleep(sleep_time)
-    GPIO.output(ggpio_output, GPIO.LOW)
+    GPIO.output(gpio_output, GPIO.LOW)
 
 def main():
     global MySQL_DB_Conn
@@ -385,7 +390,7 @@ def main():
         GPIO.cleanup()
         sys.exit(1)
     #### MAIN GAME LOGIC STARTS HERE ####
-    try:        
+    try:
         global PROG_NAME
         global PROG_VERSION
         global SETTINGS
@@ -420,14 +425,14 @@ def main():
                 if chosenanswerid == row.answer_id:
                     if NON_GPIO_ENABLED == False:
                         illuminate_lcd(SETTINGS['GPIO_GREEN_LED'], LED_WAIT_DELAY)
-                    output("You were CORRECT!",1,2,1,False)
+                    output("You are CORRECT!",1,2,1,False)
                     noofqcorrect = noofqcorrect + 1
                 else:
                     if NON_GPIO_ENABLED == False:
                         illuminate_lcd(SETTINGS['GPIO_RED_LED'], LED_WAIT_DELAY)
-                    output("Sorry, you were INCORRECT!",1,2,1,False)
+                    output("Sorry, you are INCORRECT!",1,2,1,False)
                     noofqincorrect = noofqincorrect + 1
-        except KeyboardInterrupt:            
+        except KeyboardInterrupt:
             display_score(noofqcorrect,noofqincorrect,noofqasked)
             output("Thanks for playing. Good-Bye!",1,2,1,True)
             MySQL_DB_Conn.close()
@@ -442,7 +447,7 @@ def main():
         MySQL_DB_Conn.close()
         GPIO.cleanup
         sys.exit(1)
-        
+
 if __name__ == "__main__":
     # Check command line arguments and handle accordingly
     handle_args()
